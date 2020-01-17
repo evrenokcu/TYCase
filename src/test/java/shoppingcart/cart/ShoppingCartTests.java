33b73c34179ce037bd837d0754a0947404c4ceb9
evren okçu <evrenokcu@evrens-MacBook-Pro.local>
2020-01-17 14:06:39 +0300

    last fixes

--- src/test/java/shoppingcart/cart/ShoppingCartTests.java
+++ src/test/java/shoppingcart/cart/ShoppingCartTests.java
@@ -1,5 +1,6 @@
 package shoppingcart.cart;
 
+import org.junit.Assert;
 import org.junit.Test;
 import shoppingcart.discount.Campaign;
 import shoppingcart.discount.Coupon;
@@ -11,4 +12,16 @@ import shoppingcart.shared.NumberOfProducts;
 
 public class ShoppingCartTests {
 
+    @Test
+    public void addingSameProductShouldIncreaseTheQuantity() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.addShoppingCartItem(Product.of("pen", Money.of(100), Category.of("stationary")), NumberOfProducts.of(2));
+        shoppingCart.addShoppingCartItem(Product.of("pen", Money.of(100), Category.of("stationary")), NumberOfProducts.of(3));
+
+        Assert.assertEquals(
+                NumberOfProducts.of(5),
+                shoppingCart.getShoppingCartItems().getOrDefault("pen", null).getProductInShoppingCart().getQuantity());
+
+
+    }
 }
